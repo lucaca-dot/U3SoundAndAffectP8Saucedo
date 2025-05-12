5e5b79d630878dbd5009079758ce63d1bcd64c99
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-05-12 14:31:41 -0700

    I finished challange 3
    Challange 3 is complete

--- P8U3SoundAndAffectSaucedo/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
+++ P8U3SoundAndAffectSaucedo/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class RepeatBackgroundX : MonoBehaviour
 {
     private Vector3 startPos;
-    private float repeatWidth;
+    public float repeat;
 
-    private void Start()
+     void Start()
     {
-        startPos = transform.position; // Establish the default starting position 
-        repeatWidth = GetComponent<BoxCollider>().size.y / 2; // Set repeat width to half of the background
+        startPos = transform.position;
     }
 
     private void Update()
     {
-        // If background moves left by its repeat width, move it back to start position
-        if (transform.position.x < startPos.x - repeatWidth)
+        
+        if (transform.position.x < startPos.x - repeat)
         {
             transform.position = startPos;
         }
